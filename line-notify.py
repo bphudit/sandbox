a947f2c78b343055cc25eec3a80b0cffa7b1d7ba
Boss, Phudit Thaksinawong <64381777421@student.chula.ac.th>
2022-01-23 18:47:09 +0700

    add line feature, add dependencies

---
+++ line-notify.py
@@ -0,0 +1,12 @@
+from curses import ACS_NEQUAL
+from fnmatch import fnmatch
+from unittest.case import _AssertWarnsContext
+
+
+fnmatchcnc
+_AssertWarnsContextanc
+an
+can
+scn
+ACS_NEQUALascn
+asciin
